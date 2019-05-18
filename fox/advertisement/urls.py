41ae267e445682ca3c120d0bfb9e3111c469e13b
Mohammad <mohammadroghani43@gmail.com>
2019-05-18 16:46:50 +0430

    add profile my_advertisements

--- fox/advertisement/urls.py
+++ fox/advertisement/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('adv_search', views.adv_search, name='adv_search'),
     path('favorites_advertisement', views.favorite_advertisement, name='favorite_advertisement'),
     path('add_advertisement', views.add_advertisement, name='add_advertisement'),
+    path('profile', views.profile, name='profile'),
+    path('my_advertisements', views.my_advertisements, name='my_advertisements'),
     path('<int:advertisement_id>/', views.advertisement_detail, name='advertisement_detail'),
     path('<int:advertisement_id>/add_favorite_advertisement', views.add_favorite_advertisement,
          name='add_favorite_advertisement'),
